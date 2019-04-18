ab78c1a5eb4443706a53ffda7a61a41feb776223
athanas <40561409+Athanax@users.noreply.github.com>
2019-04-18 21:27:04 +0300

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -6,7 +6,7 @@
 
 <div class="col-md-9 col-lg-9 col-sm-9 pull-left">
     
-    <div class="container-fluid">
+    {{-- <div class="container-fluid">
         <div class="fb-profile">
             <img align="left"  class="fb-image-lg" src="{{ URL::asset('storage/img/upl/'.$user->cover_image.'') }}" alt="Profile image example"/>
             <img align="left"  class="fb-image-profile thumbnail" src="{{ URL::asset('storage/img/upl/'.$user->profile_pic.'') }}" alt="Profile image example"/>
@@ -19,15 +19,18 @@
 
         <ul class="nav nav-pills nav-stacked col-sm-offset-3">
             {{-- <li class="active"><a href="#"><i class="fa fa-th fa-fw"></i>Menu</a></li> --}}
-            <li><a href="/profile/{{ $user->id }}/edit"><i class="fa fa-file-o fa-fw"></i>Edit Profile</a></li>
+            {{-- @if ($user->id==Auth::user()->id)
+              <li><a href="/profile/{{ $user->id }}/edit"><i class="fa fa-file-o fa-fw"></i>Edit Profile</a></li>
             
+            @endif
+           
                 
             @if ($user->role==1)
                 <li><a href="/users/create"><i class="fa fa-list-alt fa-fw"></i>Create Task</a></li>
                 <li><a href="#"><i class="fa fa-bar-chart-o fa-fw"></i>Charts</a></li>
             @endif
            
-        </ul>
+        </ul> --}} 
 
         {{-- starts here --}}
         <div class="">
@@ -39,8 +42,10 @@
                   <img class="img-circle" src="{{ URL::asset('storage/img/upl/'.$user->profile_pic.'') }}" alt="User Avatar">
                 </div>
                 <!-- /.widget-user-image -->
-                <h3 class="widget-user-username">Nadia Carmichael</h3>
-                <h5 class="widget-user-desc">Lead Developer</h5>
+                <h3 class="widget-user-username">{{ ucwords($user->name) }}</h3>
+                @if ($user->id==Auth::user()->id)
+              <h5 class="widget-user-desc">{{ $user->email }}</h5>
+              @endif
               </div>
               <div class="box-footer no-padding">
                 <ul class="nav nav-stacked">
@@ -48,6 +53,9 @@
                   <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
                   <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
                   <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
+                  @if ($user->id==Auth::user()->id)
+                  <li><a href="/profile/{{ $user->id }}/edit"><i class=""></i>Edit Profile</a></li>
+                  @endif
                 </ul>
               </div>
             </div>
